64aa104d86e6a5f9940735339aabd6e21e703363
Your Name <you@example.com>
2023-11-08 14:20:32 +0700

    start

--- Methods.cs
+++ Methods.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace worker_list
 {
@@ -11,10 +12,31 @@ namespace worker_list
     {
         public static void CreateXml() 
         {
-            if (!(File.Exists("workers.xml"))) File.WriteAllText("workers.xml", "<workers>\n</workers>");
+            if (!(File.Exists("workers.xml"))) File.WriteAllText("workers.xml", "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n<workers>\n</workers>");
+        }
+        public static void AddWorker(string departamentName, string workerName, string post, int salary, string description)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load("workers.xml");
+            XmlElement xRoot = xmlDocument.DocumentElement;
+            XmlElement xDepartament = null;
 
+            foreach (XmlElement xNode in xRoot) if (xNode.Attributes.GetNamedItem("name").Value == departamentName) xDepartament = xNode;
+
+            if (xDepartament == null) 
+            {
+                xDepartament = xmlDocument.CreateElement("departament");
+                XmlAttribute departamentAttr = xmlDocument.CreateAttribute("name");
+                XmlText text = xmlDocument.CreateTextNode(departamentName);
+
+                departamentAttr.AppendChild(text);
+                xDepartament.Attributes.Append(departamentAttr);
+                xRoot.AppendChild(xDepartament);
+            }
 
 
         }
+
+
     }
 }
