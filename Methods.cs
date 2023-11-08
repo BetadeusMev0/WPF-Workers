e8752054b7d0236801d06560ff6abcba60047110
Your Name <you@example.com>
2023-11-08 13:30:44 +0700

    first

---
+++ Methods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace worker_list
+{
+    class  Methods
+    {
+        public static void CreateXml() 
+        {
+            if (!(File.Exists("workers.xml"))) File.WriteAllText("workers.xml", "<workers>\n</workers>");
+
+
+
+        }
+    }
+}
