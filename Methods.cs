e5722628674890e1436a83a2fd19412cb541727e
BetadeusMev0 <143869608+BetadeusMev0@users.noreply.github.com>
2023-11-09 10:02:31 +0700

    Update Methods.cs

--- Methods.cs
+++ Methods.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,12 +8,56 @@ using System.Xml;
 
 namespace worker_list
 {
-    class  Methods
+    class Methods
     {
+        public enum Result 
+        {
+            Success,
+            Error
+        }
+
+
+
         public static void CreateXml() 
         {
             if (!(File.Exists("workers.xml"))) File.WriteAllText("workers.xml", "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n<workers>\n</workers>");
         }
+
+        public static Result RemoveWorker(string departament, string name) 
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("workers.xml");
+            XmlElement xRoot = xmlDoc.DocumentElement;
+
+            XmlElement xDepartament = null;
+
+
+            foreach(XmlElement xNode in xRoot) 
+            {
+                if (xNode.GetAttribute("name") == departament) xDepartament = xNode;
+            }
+            if (xDepartament == null) return Result.Error;
+
+            XmlElement workerEl = null;
+
+            foreach(XmlElement xNode in xDepartament) 
+            {
+                if (xNode.GetAttribute("name") == name) workerEl = xNode;
+            }
+
+            if (workerEl == null) return Result.Error;
+
+            xDepartament.RemoveChild(workerEl);
+
+
+            xmlDoc.Save("workers.xml");
+            return Result.Success;
+        }
+
+
+
+
+
         public static void AddWorker(string departamentName, string workerName, string post, int salary, string description)
         {
             XmlDocument xmlDocument = new XmlDocument();
@@ -33,8 +77,30 @@ namespace worker_list
                 xDepartament.Attributes.Append(departamentAttr);
                 xRoot.AppendChild(xDepartament);
             }
+            var workerEl = xmlDocument.CreateElement("worker");
+            var workerNameAttr = xmlDocument.CreateAttribute("name");
+            var workerNameAttrText = xmlDocument.CreateTextNode(workerName);
+            workerNameAttr.AppendChild(workerNameAttrText);
 
+            var workerPost = xmlDocument.CreateAttribute("post");
+            var workerPostText = xmlDocument.CreateTextNode(post);
+            workerPost.AppendChild(workerPostText);
 
+            var workerSalary = xmlDocument.CreateAttribute("salary");
+            var workerSalaryText = xmlDocument.CreateTextNode(salary.ToString());
+            workerSalary.AppendChild(workerSalaryText);
+           
+            var workerDescription = xmlDocument.CreateAttribute("description");
+            var workerDescriptionText = xmlDocument.CreateTextNode(description);
+            workerDescription.AppendChild(workerDescriptionText);
+
+            workerEl.Attributes.Append(workerNameAttr);
+            workerEl.Attributes.Append(workerPost);
+            workerEl.Attributes.Append(workerSalary);
+            workerEl.Attributes.Append(workerDescription);
+
+            xDepartament.AppendChild(workerEl);
+            xmlDocument.Save("workers.xml");
         }
 
 
